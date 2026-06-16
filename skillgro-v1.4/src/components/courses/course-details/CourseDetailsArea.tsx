a3823e05a405db7faee583dac6543661eb94601a
tzarventure <crownleaf@tzar.co>
2026-06-16 13:57:20 +0530

    fix meta1 error

--- skillgro-v1.4/src/components/courses/course-details/CourseDetailsArea.tsx
+++ skillgro-v1.4/src/components/courses/course-details/CourseDetailsArea.tsx
@@ -1,55 +1,60 @@
-"use client"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import Overview from "./Overview";
 import Sidebar from "./Sidebar";
-import Curriculum from "./Curriculum"
-import Reviews from "./Reviews"
-import Instructors from "./Instructors"
+import Curriculum from "./Curriculum";
+import Reviews from "./Reviews";
+import Instructors from "./Instructors";
 import AdvanceDMOverview from "./Overview/AdvanceDMOverview";
 import AdvanceDMCurriculum from "./Curriculum/AdvanceDMCurriculum";
 import AdvanceDMSidebar from "./Sidebar/AdvanceDMSidebar";
-import course_details_img1 from "@/assets/img/courses/courses_details.jpg"
-import course_details_img2 from "@/assets/img/courses/course_author001.png"
+import course_details_img1 from "@/assets/img/courses/courses_details.jpg";
+import course_details_img2 from "@/assets/img/courses/course_author001.png";
 
 const tab_title: string[] = ["Overview", "Curriculum"];
 
 const CourseDetailsArea = ({ single_course }: any) => {
-
   const [activeTab, setActiveTab] = useState(0);
 
   const handleTabClick = (index: number) => {
     setActiveTab(index);
   };
 
-  const currentOverView = () =>{
-    if(single_course?.overview === "certification-in-advanced-digital-marketing-&-ai"){
-      return <AdvanceDMOverview />
+  const currentOverView = () => {
+    if (
+      single_course?.overview ===
+      "certification-in-advanced-digital-marketing-&-ai"
+    ) {
+      return <AdvanceDMOverview />;
+    } else {
+      return <Overview />;
     }
-      else{
-        return <Overview />
-      }
-  }
+  };
 
-  const currentCurriculum = () =>{
-    if(single_course?.overview === "certification-in-advanced-digital-marketing-&-ai"){
-      return <AdvanceDMCurriculum />
+  const currentCurriculum = () => {
+    if (
+      single_course?.overview ===
+      "certification-in-advanced-digital-marketing-&-ai"
+    ) {
+      return <AdvanceDMCurriculum />;
+    } else {
+      return <Curriculum />;
     }
-      else{
-        return <Curriculum />
-      }
-  }
+  };
 
-  const currentSidebar = () =>{
-    if(single_course?.overview === "certification-in-advanced-digital-marketing-&-ai"){
-      return <AdvanceDMSidebar />
+  const currentSidebar = () => {
+    if (
+      single_course?.overview ===
+      "certification-in-advanced-digital-marketing-&-ai"
+    ) {
+      return <AdvanceDMSidebar />;
+    } else {
+      return <Sidebar />;
     }
-      else{
-        return <Sidebar />
-      }
-  }
- 
+  };
+
   return (
     <section className="courses__details-area section-py-120">
       <div className="container">
@@ -61,37 +66,81 @@ const CourseDetailsArea = ({ single_course }: any) => {
             <div className="courses__details-content">
               <ul className="courses__item-meta list-wrap">
                 <li className="courses__item-tag">
-                  <Link href="/courses">{single_course?.category ? single_course.category : "Development"}</Link>
+                  <Link href="/courses">
+                    {single_course?.category
+                      ? single_course.category
+                      : "Development"}
+                  </Link>
                 </li>
-                <li className="avg-rating"><i className="fas fa-star"></i>{single_course?.rating ? single_course.rating : "(4.5 Reviews)"}</li>
+                <li className="avg-rating">
+                  <i className="fas fa-star"></i>
+                  {single_course?.rating
+                    ? single_course.rating
+                    : "(4.5 Reviews)"}
+                </li>
               </ul>
-              <h2 className="title">{single_course?.title ? single_course.title : "Resolving Conflicts Between Designers And Engineers"}</h2>
+              <h2 className="title">
+                {single_course?.title
+                  ? single_course.title
+                  : "Resolving Conflicts Between Designers And Engineers"}
+              </h2>
               <div className="courses__details-meta">
-                <ul className="list-wrap">
-                  <li>{single_course.meta1}</li>
-                  <li>{single_course.meta2}</li>
-                  <li>{single_course.meta3}</li>
-                </ul>
+                {single_course && (
+                  <ul className="list-wrap">
+                    {single_course.meta1 && <li>{single_course.meta1}</li>}
+                    {single_course.meta2 && <li>{single_course.meta2}</li>}
+                    {single_course.meta3 && <li>{single_course.meta3}</li>}
+                  </ul>
+                )}
               </div>
               <ul className="nav nav-tabs" id="myTab" role="tablist">
                 {tab_title.map((tab, index) => (
-                  <li key={index} onClick={() => handleTabClick(index)} className="nav-item" role="presentation">
-                    <button className={`nav-link ${activeTab === index ? "active" : ""}`}>{tab}</button>
+                  <li
+                    key={index}
+                    onClick={() => handleTabClick(index)}
+                    className="nav-item"
+                    role="presentation"
+                  >
+                    <button
+                      className={`nav-link ${activeTab === index ? "active" : ""}`}
+                    >
+                      {tab}
+                    </button>
                   </li>
                 ))}
               </ul>
               <div className="tab-content" id="myTabContent">
-                <div className={`tab-pane fade ${activeTab === 0 ? 'show active' : ''}`} id="overview-tab-pane" role="tabpanel" aria-labelledby="overview-tab">
+                <div
+                  className={`tab-pane fade ${activeTab === 0 ? "show active" : ""}`}
+                  id="overview-tab-pane"
+                  role="tabpanel"
+                  aria-labelledby="overview-tab"
+                >
                   {/* <Overview /> */}
                   {currentOverView()}
                 </div>
-                <div className={`tab-pane fade ${activeTab === 1 ? 'show active' : ''}`} id="overview-tab-pane" role="tabpanel" aria-labelledby="overview-tab">
+                <div
+                  className={`tab-pane fade ${activeTab === 1 ? "show active" : ""}`}
+                  id="overview-tab-pane"
+                  role="tabpanel"
+                  aria-labelledby="overview-tab"
+                >
                   {currentCurriculum()}
                 </div>
-                <div className={`tab-pane fade ${activeTab === 2 ? 'show active' : ''}`} id="overview-tab-pane" role="tabpanel" aria-labelledby="overview-tab">
+                <div
+                  className={`tab-pane fade ${activeTab === 2 ? "show active" : ""}`}
+                  id="overview-tab-pane"
+                  role="tabpanel"
+                  aria-labelledby="overview-tab"
+                >
                   <Instructors />
                 </div>
-                <div className={`tab-pane fade ${activeTab === 3 ? 'show active' : ''}`} id="overview-tab-pane" role="tabpanel" aria-labelledby="overview-tab">
+                <div
+                  className={`tab-pane fade ${activeTab === 3 ? "show active" : ""}`}
+                  id="overview-tab-pane"
+                  role="tabpanel"
+                  aria-labelledby="overview-tab"
+                >
                   <Reviews />
                 </div>
               </div>
@@ -101,7 +150,7 @@ const CourseDetailsArea = ({ single_course }: any) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CourseDetailsArea
+export default CourseDetailsArea;
