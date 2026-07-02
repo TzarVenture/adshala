94246a1f287396b797a4c7eac1f068aab6eb2294
tzarventure <crownleaf@tzar.co>
2026-07-02 11:28:18 +0530

    change name

--- skillgro-v1.4/src/components/homes/home-one/Banner.tsx
+++ skillgro-v1.4/src/components/homes/home-one/Banner.tsx
@@ -14,7 +14,7 @@ import banner_icon_1 from '@/assets/img/banner/bg_dots.svg';
 import banner_author_1 from '@/assets/img/banner/banner_author01.png';
 import banner_author_2 from '@/assets/img/banner/banner_author02.png';
 
-   import bg_img from '@/assets/img/banner/banner_bg.png';
+import bg_img from '@/assets/img/banner/banner_bg.png';
 
 const Banner: React.FC = () => {
 
@@ -22,14 +22,14 @@ const Banner: React.FC = () => {
 
    return (
       <section
-         className="banner-area banner-bg tg-motion-effects" style={{ backgroundImage: `url(${bg_img.src})`}}>
+         className="banner-area banner-bg tg-motion-effects" style={{ backgroundImage: `url(${bg_img.src})` }}>
          <div className="container">
             <div className="row justify-content-between align-items-start">
                <div className="col-xl-5 col-lg-6">
                   <div className="banner__content">
                      <h3 className="title tg-svg">
                         100% Job
-                        <span className="position-relative" style={{marginLeft:"10px"}}>
+                        <span className="position-relative" style={{ marginLeft: "10px" }}>
                            <span className="svg-icon"></span>
                            <svg x="0px" y="0px" preserveAspectRatio="none" viewBox="0 0 209 59" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565" fill="currentcolor" />
@@ -37,30 +37,30 @@ const Banner: React.FC = () => {
                            Placement
                         </span>
                         <br />
-                        1 Month <b>Paid Internship</b> 
+                        1 Month <b>Paid Internship</b>
                      </h3>
                      <p>
                         Industry-recognized certification programs with real time projects with paid internship.
                      </p>
                      <div className="banner__btn-wrap">
-             <button
-  type="button"
-  className="btn arrow-btn"
-  onClick={() => {
-    document
-      .getElementById("enquiry-form01")
-      ?.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-  }}
->
-  Enquire Now <BtnArrow />
-</button>
+                        <button
+                           type="button"
+                           className="btn arrow-btn"
+                           onClick={() => {
+                              document
+                                 .getElementById("enquiry-form01")
+                                 ?.scrollIntoView({
+                                    behavior: "smooth",
+                                    block: "start",
+                                 });
+                           }}
+                        >
+                           Enquire Now <BtnArrow />
+                        </button>
                      </div>
                   </div>
                </div>
-               
+
                <div className="col-lg-6">
                   <div className="banner__images">
                      <Image src={banner_img_1} alt="img" className="main-img" />
@@ -74,7 +74,7 @@ const Banner: React.FC = () => {
                            <div className="image">
                               <Image src={banner_author_1} alt="img" />
                            </div>
-                           <h6 className="name">Sumit Gupta</h6>
+                           <h6 className="name">Riya Gupta</h6>
                         </div>
                         <div className="banner__author-item">
                            <div className="image">
